efbfb5efbe5bb337de997bf65e65667a0a3c2026
Spencer King <spencerking@users.noreply.github.com>
2019-07-25 22:20:43 -0500

    Some books use html, not xhtml

--- e2t.sh
+++ e2t.sh
@@ -9,7 +9,7 @@ EPUB=$1
 
 CONTENT=$(unzip -l "$EPUB" | grep -o "[A-Za-z/]*content.opf")
 
-FILES=$(unzip -c "$EPUB" "$CONTENT" | grep -o "[A-Za-z0-9_]*[.]xhtml")
+FILES=$(unzip -c "$EPUB" "$CONTENT" | grep -o "[A-Za-z0-9_]*[.][x]\{0,1\}html")
 
 for file in $FILES ; do
     #echo "$file"
