9852355d5365e06f565e2415d984cd1d292b3d2a
Spencer King <spencerking@users.noreply.github.com>
2019-07-31 22:22:15 -0500

    Use awk to remove remaining css

--- e2t.sh
+++ e2t.sh
@@ -12,7 +12,8 @@ CONTENT=$(unzip -l "$EPUB" | grep -o "[A-Za-z/]*content.opf")
 FILES=$(unzip -c "$EPUB" "$CONTENT" | grep -o "[A-Za-z0-9_]*[.][x]\{0,1\}html")
 
 for file in $FILES ; do
-    #echo "$file"
     TEXT=$(unzip -l "$EPUB" | grep -o "[A-Za-z0-9_/]*$file")
-    unzip -c "$EPUB" "$TEXT" | sed 's/<\/p>/\'$'\n''/g' | sed 's/<[^>]*>/ /g' | sed "s/Archive:  $EPUB//g" | sed "s/inflating: [A-Za-z0-9_\/]*$file//g" | sed 's/<style>[^>]*<\/style>//g' >> temp.txt
+    unzip -c "$EPUB" "$TEXT" | sed 's/<\/p>/\'$'\n''/g' | sed "s/Archive:  $EPUB//g" | sed "s/inflating: [A-Za-z0-9_\/]*$file//g" >> temp.txt
+    awk '/<style>/{p=1;print}/<\/style>/{p=0}!p' temp.txt | sed 's/<[^>]*>/ /g' >> "$EPUB".txt
+    rm temp.txt
 done
